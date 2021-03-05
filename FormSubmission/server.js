06a8af390caf8f1f219c2e789cae3fa69fb268b2
davidjosephind <davidjosephind@gmail.com>
2021-03-05 17:03:49 +0530

    Fix bad css routing

--- FormSubmission/server.js
+++ FormSubmission/server.js
@@ -5,13 +5,16 @@ const app = express();
 // Body-parser
 app.use(express.urlencoded({ extended: true }));
 
+// Serve public files
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/register.html");
+  res.sendFile(__dirname + "/views/register.html");
 });
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  res.redirect("/");
+  res.send(`Hello ${req.body.email}`);
 });
 
 app.listen(3000, () => {
